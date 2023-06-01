286fa01bf2462fba20265887b951b7df6064c68a
Sn-G-m <133529150+Sn-G-m@users.noreply.github.com>
2023-06-02 03:04:29 +0530

    Update Extraction.py

--- Extraction.py
+++ Extraction.py
@@ -1,8 +1,8 @@
-#importing packages
-from imutils import paths
-import numpy as num
-import argparse
+#importing modules
+from imutils import paths     #Basic image processing functions
 import imutils
-import pickle
-import cv2
+import numpy as num           #For processing and manipulation of data
+import argparse               #For command-line interface
+import pickle                 #For serialising and de-serialising a python object structure
+import cv2                    #An opencv interface for face detection and recogniton
 import os
