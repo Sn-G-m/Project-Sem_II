dd18b47a417e0beeb2dd5c246bf61506232a2dd6
Sn-G-m <133529150+Sn-G-m@users.noreply.github.com>
2023-06-03 13:00:25 +0530

    Update Extraction.py
    Complete code for data extraction from dataset

--- Extraction.py
+++ Extraction.py
@@ -34,6 +34,65 @@ embedding_model = cv2.dnn.readNetFromTorch(emb_model_path)           # Reading a
 # Image location for training as a list
 data = arg_dict("data_set")
 img_path = list(paths.list_images(data))
+print("<< Image location retrieving >>")
+
+# Container for data
+facial_embs = []
+face_names = []
+
+N_FACES = 0             # Number of proper images
+
+# Iterating through the files
+for (serial_no, location) in enumerate(img_path):
+    print("Processing Image, iteratiion {}/{}".format((serial_no + 1), len(img_path)))
+    person = location.split(os.path.sep)[-2]
+
+    # Loading images
+    image = cv2.imread(location)
+    image = imutils.resize(image, width = 600)
+    (height, width) = image.shape[ : 2]
+
+    #Constructing a vector dimensional array of the image
+    img_tensor = cv2.dnn.BlobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB = False, crop = False)
+
+    #Locating face with Deep Neural Network Model 
+    face_detector.setInput(img_tensor)
+    # Output
+    detect_output = face_detector.forward()
+
+    if len(detect_output) > 0:             # Atleast one face detection
+        index = num.argmax(detect_output[0, 0, :, 2])     # Getting the bounding boxes and confidence level
+        confidence = detect_output([0, 0, index, 2])
+        
+        if confidence >= arg_dict["confidence_level"]:
+            # Getting coordinates and re-scaling
+            limits = detect_output[0, 0, index, 3 : 7] * num.array([width, height, width, height])
+            (x_1, y_1, x_2, y_2) = limits.astype("int")           # Pixel positions
+
+            face = image[y_1 : y_2, x_1 : x_2]
+            face_height, face_width = face.shape[ : 2]
+
+            if face_height < 20 or face_width < 20:
+                continue
+
+            face_tensor = cv2.dnn.BlobFromImage(face, 1.0/255,
+                                                (96,96), (0, 0, 0),swapRB = True, crop = False)
+            embedding_model.setInput(face_tensor)
+            vector = embedding_model.forward()
+
+            face_names.append(person)
+            facial_embs.append(vector.flatten())
+
+            N_FACES += 1
+
+# Saving data to a file
+face_data = {"vector_file" : facial_embs, "names" : face_names} 
+file = open(arg_dict["vector_file"], "wb")
+file.write(pickle.dumps(face_data))
+file.close()
+
+
+
 
 
 
