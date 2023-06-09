f220e19c286e70880a4085fc55f0acb277533d72
Sn-G-m <133529150+Sn-G-m@users.noreply.github.com>
2023-06-09 23:16:39 +0530

    Update train_data.py

--- train_data.py
+++ train_data.py
@@ -8,21 +8,21 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 
 
-
 # File paths
 def train():
     DEFAULT_VECTOR = (r"C:\Users\DELL\AppData\Local\Programs\Python\Python311"
-                    r"\SemIIProject\Sample2\Final_Project\output"
-                    r"\ExtractedData.PICKLE")
-    DEFAULT_RECOGNITION = (r"C:\Users\DELL\AppData\Local\Programs\Python\Python311"
-                        r"\SemIIProject\Sample2\Final_Project\output"
-                        r"\Recognitions.pickle")
+                      r"\SemIIProject\Sample2\Final_Project\output"
+                      r"\ExtractedData.PICKLE")
+    DEFAULT_RECOGNITION = (r"C:\Users\DELL\AppData\Local\Programs\Python"
+                           r"\Python311\SemIIProject\Sample2\Final_Project"
+                           r"\output\Recognitions.pickle")
     DEFAULT_LABELS = (r"C:\Users\DELL\AppData\Local\Programs\Python\Python311"
-                    r"\SemIIProject\Sample2\Final_Project\output"
-                    r"\LabelData.pickle")
+                      r"\SemIIProject\Sample2\Final_Project\output"
+                      r"\LabelData.pickle")
 
     # Adding arguments
-    arg_parser = argparse.ArgumentParser()  # Creating an ArgumentParser instance
+    # Creating an ArgumentParser instance
+    arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument("-vf", "--vector_file", default=DEFAULT_VECTOR)
     arg_parser.add_argument("-rd", "--recognition_data",
@@ -32,7 +32,6 @@ def train():
     arg_dict = vars(arg_parser.parse_args())
 
     # Face data loading from extract file
-    #facial_data = pickle.loads(open(arg_dict["vector_file"], "rb").read())
     with open(arg_dict["vector_file"], "rb") as file:
         facial_data = pickle.load(file)
     print("<< Face extractions loading complete >>")
